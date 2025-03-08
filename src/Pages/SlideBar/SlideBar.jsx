a66406ca500c99b3486d022bfd2c8af99c330044
mnla8821 <mnla8821@gmail.com>
2025-03-08 18:29:58 +0300

    with medea2

--- src/Pages/SlideBar/SlideBar.jsx
+++ src/Pages/SlideBar/SlideBar.jsx
@@ -38,27 +38,19 @@ const SlideBar = () => {
 
   return (
     <div className="w-full bg-[var(--white-blue)] h-auto min-h-[120vh] flex flex-col  items-center max-md:flex-col ">
-      <h1 className="font-bold text-[var(--blue)] text-4xl leading-10 p-14 title-slide">
+      <h1 className="font-bold text-[var(--blue)] text-4xl leading-10 p-14 title-slide max-sm:h-[30vh] ">
         مجالات الكورسات المتاحة
       </h1>
       {/* Begin a slider  */}
-      <div className="w-full h-[100vh] flex items-end max-md:items-center max-md:px-10 max-md:h-[140vh] max-sm:h-[120vh]">
+      <div className="w-full parent-swiper h-[100vh] flex items-end max-md:items-center max-md:px-10 max-md:h-[140vh] max-sm:h-[100vh] max-sm:px-4 ">
         <Swiper
           navigation={true}
           modules={[Navigation]}
           slidesPerView={valueOfWidth == true ? 1 : 3.5}
           spaceBetween={80}
-          className="mySwiper w-full min-h-[80%]  "
+          className="mySwiper w-full min-h-[80%]  max-sm:h-auto "
         >
-          <SwiperSlide className="  bg-[var(--light-blue)]  rounded-2xl flex justify-center items-center p-5 max-md:flex-col ">
-            <div className="bg-[var(--sugar)] pt-4 px-1.5 pb-0.5 rounded-xl">
-              <img src={img4} alt="" className="h-fit w-full " />
-            </div>
-            <h4 className="w-full p-3 text-center text-black font-semibold text-2xl">
-              الطب والصحة
-            </h4>
-          </SwiperSlide>
-          <SwiperSlide className="  bg-[var(--light-blue)]  rounded-2xl flex justify-center items-center p-5">
+          <SwiperSlide className="  bg-[var(--light-blue)]  rounded-2xl flex justify-center items-center p-5 ">
             <div className="bg-[var(--sugar)] pt-4 px-1.5 pb-0.5 rounded-xl">
               <img src={img7} alt="" className="h-fit w-full " />
             </div>
@@ -80,6 +72,14 @@ const SlideBar = () => {
             </div>
             <h4 className="w-full p-3 text-center text-black font-semibold text-2xl">
               التصميم والفنون
+            </h4>
+          </SwiperSlide>
+          <SwiperSlide className="  bg-[var(--light-blue)]  rounded-2xl flex justify-center items-center p-5  ">
+            <div className="bg-[var(--sugar)] pt-4 px-1.5 pb-0.5 rounded-xl">
+              <img src={img4} alt="" className="h-fit w-full " />
+            </div>
+            <h4 className="w-full p-3 text-center text-black font-semibold text-2xl">
+              الطب والصحة
             </h4>
           </SwiperSlide>
           <SwiperSlide className="  bg-[var(--light-blue)]  rounded-2xl flex justify-center items-center p-5">
